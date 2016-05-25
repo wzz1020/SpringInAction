0a8318b5c42574598ed9075c24e142aea34dfc97
henyumen <614173971@qq.com>
2016-05-25 10:49:04 +0800

    Initial commit

---
+++ src/main/java/chapter_13_jmx/Hello.java
@@ -0,0 +1,53 @@
+package chapter_13_jmx;
+
+import com.sun.jmx.mbeanserver.MXBeanMappingFactory;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
+/**
+ * Hello
+ *
+ * @author FD
+ * @date 2016/5/25
+ */
+public class Hello implements HelloMBean{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Hello World, " + name);
+    }
+
+    public static void main(String[] args) {
+        try {
+//            MBeanServer server = MBeanServerFactory.createMBeanServer();
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            server.registerMBean(new Hello(), new ObjectName("fd:name=hello"));
+
+//            HtmlAdaptorServer adapter = new HtmlAdaptorServer();
+//            server.registerMBean(adapter, new ObjectName("fd:name=adapter"));
+//
+//            adapter.start();
+            while (true) {
+
+            }
+        } catch (InstanceAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (MBeanRegistrationException e) {
+            e.printStackTrace();
+        } catch (NotCompliantMBeanException e) {
+            e.printStackTrace();
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/chapter_13_jmx/HelloMBean.java
@@ -0,0 +1,13 @@
+package chapter_13_jmx;
+
+/**
+ * HelloMBean
+ *
+ * @author FD
+ * @date 2016/5/25
+ */
+public interface HelloMBean {
+    public String getName();
+    public void setName(String name);
+    public void printHello();
+}
